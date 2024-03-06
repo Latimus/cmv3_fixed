a6e882b188d78385665cfe857ff043a4a98a1f2c
plancedev <paul.brooks1318@gmail.com>
2024-03-06 01:53:10 -0600

    first commit

---
+++ settings.tsx
@@ -0,0 +1,35 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+  {
+    label: "WL",
+    mintText: "WL mint paying with CIF - 15,0000",
+    buttonLabel: "Mint now!",
+    header: "WL Mint",
+  },
+  {
+    label: "OG",
+    mintText: "Mint Paying with ABC",
+    buttonLabel: "Mint now!",
+    header: "OG Mint",
+  },
+  {
+    label: "Public",
+    mintText: "Mint Paying with CIF - 20,000",
+    buttonLabel: "Mint now!",
+    header: "Public Mint",
+  },
+  {
+    label: "default",
+    mintText: "default mintText",
+    buttonLabel: "Mint now!",
+    header: "Default",
+  },
+];
+
+//header image in the ui. replace with your own
+export const image =
+  "https://arweave.net/VTvxsbCuVsiWdGW5djZRARpwhWCkROY-HHMNJ6FWi0U?ext=png";
+
+//website title
+export const headerText = "Catwifhat Mint";
